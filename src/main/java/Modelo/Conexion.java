ef341317bf8face09bfd5562ca72855387bd6ed5
David <vjdavid621@gmail.com>
2020-04-06 03:52:43 -0700

    adding conexion class

--- src/main/java/Modelo/Conexion.java
+++ src/main/java/Modelo/Conexion.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package Modelo;
+import java.sql.*;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author david
  */
 public class Conexion {
-    
+    Connection conectar = null;
+   
+    public Connection conexion() {
+        try {
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/Tienda?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "david12345");
+            System.out.println("Conexion Exitosa");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conectar;
+    }
 }
