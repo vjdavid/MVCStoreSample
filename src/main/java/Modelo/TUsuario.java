defed1e98004e6ac527754b4ab06e4631c3d0f13
David <vjdavid621@gmail.com>
2020-04-06 03:52:55 -0700

    adding model of Client

--- src/main/java/Modelo/TUsuario.java
+++ src/main/java/Modelo/TUsuario.java
@@ -10,5 +10,70 @@ package Modelo;
  * @author david
  */
 public class TUsuario {
+    private int id;
+    private String codigo;
+    private String nombre;
+    private String telefono;
+    private String usuario;
+    private String contrasena;
     
+    public TUsuario(){
+    }
+
+    public TUsuario(String codigo, String nombre, String telefono, String usuario, String contrasena) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }            
 }
